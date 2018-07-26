d6ab4ce4e3ab8f655fa3477630add007a7eb6551
刘志国 <liuzg8@asiainfo.com>
2018-07-26 15:39:05 +0800

    增加模版选择

--- src/actions/init.ts
+++ src/actions/init.ts
@@ -12,38 +12,48 @@ export default function (name: string, options: any) {
   if (!fs.existsSync(name)) {
     if (options.type) {
       if (templateList.indexOf(options.type) !== -1) {
-        inquirer.prompt([
-          {name: 'description', message: '请输入项目描述:'},
-          {name: 'author', message: '请输入作者名称:'}
-        ]).then((answers: any) => {
-          const spinner = ora('正在下载模版...')
-          spinner.start()
-          download(setting.gitPath + '#' + options.type, name, err => {
-            if (err) {
-              spinner.fail()
-              showFail('模版下载失败！')
-            } else {
-              spinner.succeed()
-              const fileName = `${name}/package.json`;
-              const meta = {
-                name,
-                description: answers.description,
-                author: answers.author
-              }
-              if (fs.existsSync(fileName)) {
-                const content = fs.readFileSync(fileName).toString();
-                const result = handlebars.compile(content)(meta);
-                fs.writeFileSync(fileName, result);
-              }
-              showSuccess('项目初始化完成')
-            }
-          })
-        })
+        initProject(options.type)
       } else {
         showFail('没有对应类型的模版！')
       }
+    } else {
+      inquirer.prompt([
+        {type: 'list', name: 'type', message: '请选择模版:', choices: templateList}
+      ]).then((answers: any) => {
+        initProject(answers.type)
+      })
     }
   } else {
     showFail('项目已存在！')
   }
+}
+
+function initProject(type: string) {
+  inquirer.prompt([
+    {name: 'description', message: '请输入项目描述:'},
+    {name: 'author', message: '请输入作者名称:'}
+  ]).then((answers: any) => {
+    const spinner = ora('正在下载模版...')
+    spinner.start()
+    download(setting.gitPath + '#' + type, name, err => {
+      if (err) {
+        spinner.fail()
+        showFail('模版下载失败！')
+      } else {
+        spinner.succeed()
+        const fileName = `${name}/package.json`;
+        const meta = {
+          name,
+          description: answers.description,
+          author: answers.author
+        }
+        if (fs.existsSync(fileName)) {
+          const content = fs.readFileSync(fileName).toString();
+          const result = handlebars.compile(content)(meta);
+          fs.writeFileSync(fileName, result);
+        }
+        showSuccess('项目初始化完成')
+      }
+    })
+  })
 }
